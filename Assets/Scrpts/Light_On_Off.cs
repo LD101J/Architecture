59cfd06112f0b1267d199c744a611fa5ac6cadd1
LD101 <jmuendongei@gmail.com>
2023-03-28 09:17:54 +0300

    Creating Repo

---
+++ Assets/Scrpts/Light_On_Off.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Migaa_Golf_Estate
+{
+    /// <summary>
+    /// This class will just be for turning lights on and off
+    /// </summary>
+    public class Light_On_Off : MonoBehaviour
+    {
+        #region Psuedocode for Light On and Off
+        // i need a boolean to check if the switch is on or off
+        // i also need an if statement, that will do something if these bool is true or false
+        //this if statement will be on update
+        #endregion
+        #region Psuedocode for moving the player around
+        //i need a variable float for movespeed
+        //
+        #endregion
+
+        [SerializeField] private bool lightOn;
+
+        private void Update()
+        {
+           
+        }
+    }
+}
